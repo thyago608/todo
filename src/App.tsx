72201a558a6fa5f144b1c3470eba1049c711ce1e
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-10-28 14:28:12 -0400

    Created and Styled Component Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { Task } from "./components/Task";
+import "./styles/global.css";
+
 export function App() {
-  return <div>Hello Todo</div>;
+  return <Task />;
 }
---
+++ src/components/Task/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Trash, Check } from "phosphor-react";
+import styles from "./styles.module.css";
+
+export function Task() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <div className={styles.container}>
+      <button
+        type="button"
+        className={`${
+          isChecked ? styles.checkedButton : styles.unCheckedButton
+        }`}
+        onClick={() => setIsChecked(!isChecked)}
+      >
+        <div className={styles.containerIcon}>
+          <Check size={18} weight="bold" />
+        </div>
+        <p>
+          Integer urna interdum massa libero auctor neque turpis turpis semper.
+          Duis vel sed fames integer.
+        </p>
+      </button>
+
+      <button type="button" className={styles.deleteButton}>
+        <Trash size={14} weight="bold" />
+      </button>
+    </div>
+  );
+}
