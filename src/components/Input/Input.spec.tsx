befdd3bac6cae82f633475d41222ab4676858071
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-01 14:24:49 -0400

    Added Testing CreateButton Component

--- src/components/Input/Input.spec.tsx
+++ src/components/Input/Input.spec.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Input } from ".";
 
-describe("Input", () => {
+describe("Input Component", () => {
   it("should render correctly", () => {
     render(<Input />);
     expect(screen.getByRole("textbox")).toBeInTheDocument();
